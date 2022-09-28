00420404d2feaef8109fb52fbe9771d5e5ae319e
田き <prgrm2960.17chan@gmail.com>
2022-09-29 01:33:20 +0900

    プロジェクト ファイルを追加します。

---
+++ Assets/Sample/KaleidScopedPosition/CircleMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+
+public class CircleMove : MonoBehaviour
+{
+
+    public Transform TF;
+    float rotate;
+    int angularVelocity;
+
+    // Use this for initialization
+    void Awake()
+    {
+        TF = transform;
+        rotate = Random.Range(0, 360f);
+        angularVelocity = Random.Range(2, 9);
+    }
+
+    // Update is called once per frame
+    public void Move()
+    {
+        rotate = (rotate + angularVelocity) % 360;
+
+        float angle = rotate * Mathf.Deg2Rad;
+
+        TF.position += new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0)*0.2f;
+
+    }
+}
---
+++ Assets/TakiExtension/CompletableObservable/ObservableExtension.cs
@@ -0,0 +1,33 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniRx;
+
+namespace TakiExtension
+{
+    public static class ObservableExtension
+    {
+
+        public static IObservable<T> GetObservableAcync<T>(this IObservable<T> inStream,Func<UniTask> func)
+        {
+            //入力されたストリームから、実行完了すると完了を通知する自身の新しいストリームを生成する
+            IObservable<T> stream = Observable.Create<T>(observer => 
+            {
+                IDisposable subscription = inStream.Subscribe(async x => 
+                {
+                    UnityEngine.Debug.Log("stream start");
+                    await func();
+                    observer.OnCompleted();
+                });
+                return subscription;
+            });
+
+            return stream;
+        }
+
+
+    }
+}
