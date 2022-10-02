0a3e855fe8e9770b2887156083e9be4f002cd30f
田き <prgrm2960.17chan@gmail.com>
2022-10-02 23:03:17 +0900

    汎用シングルトンの作成(未検証版)

---
+++ Assets/TakiExtension/Singleton/BehaviourSingleton.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+//まだ未検証です
+namespace Assets.TakiExtension.Singleton
+{
+    public abstract class BehaviourSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+
+        private static T instance;
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+
+                    Type t = typeof(T);
+
+                    instance = (T)FindObjectOfType(t);
+                    if (instance == null)
+                    {
+                        Debug.LogError(t + " をアタッチしているGameObjectはありません");
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        virtual protected void Awake()
+        {
+
+            if(instance == null)
+            {
+                instance = GetComponent<T>();
+                DontDestroyOnLoad(this.gameObject);
+                LateAwake();
+            }
+            else if (this == Instance)
+            {
+                LateAwake();
+                return;
+            }
+            else
+            {
+                Debug.LogError(
+                    typeof(T) +
+                    " は既に他のGameObjectにアタッチされているため、コンポーネントを破棄しました." +
+                    " アタッチされているGameObjectは " + Instance.gameObject.name + " です.");
+                Destroy(this);
+            }
+
+            // なんとかManager的なSceneを跨いでこのGameObjectを有効にしたい場合は
+            // ↓コメントアウト外してください.
+        }
+
+        protected abstract void LateAwake();
+    }
+}
